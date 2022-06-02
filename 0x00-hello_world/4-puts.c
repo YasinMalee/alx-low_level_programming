d551071bc837d0bf4d388b2c8d557064ada5fb31
Yasin <salim.malee@gmail.com>
2022-06-02 13:19:04 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-void  main()
+void main()
 {
-puts("\"Programming is like buildingi a multilingual puzzle");
-return (0);i
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
