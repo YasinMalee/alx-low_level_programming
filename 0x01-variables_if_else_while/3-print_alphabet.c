0af728df7b1ed642d91a8985cc33b4c62e01b204
Yasin <salim.malee@gmail.com>
2022-06-04 07:09:22 -0700

    3-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+        putchar(n);
+}
+putchsr('\n');
+
+        return (0);
+
+}
