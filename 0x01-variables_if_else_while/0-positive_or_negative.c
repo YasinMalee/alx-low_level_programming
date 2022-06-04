1612e72fd63b2aaebe005d821b8cbae389f78bc0
Yasin <salim.malee@gmail.com>
2022-06-04 01:19:44 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@ int main(void)
 {
 int n;
 srand(time(0));
-n = rand () - RAND_MAX / 2;
+n = rand - RAND_MAX / 2;
 /* this is your code*/
 if (n % 10 > 5)
 printf("last digit of %d is %d and is greater than 5\n",n, n % 10);
