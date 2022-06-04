5d11cd1311c1a9969cc55477a651dd944a34ce4a
Yasin <salim.malee@gmail.com>
2022-06-04 05:51:27 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("last digit of %d is positive\n", n);
+printf("%d is positive\n", n);
 else if (n == 0)
 printf("%d is zero\n", n);
 else
