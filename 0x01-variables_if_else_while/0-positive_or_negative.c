768941b322062da3389c23d42738c0ab0f24f3b5
Yasin <salim.malee@gmail.com>
2022-06-04 05:05:27 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int ln = n % 10;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (ln > 5)
-printf("last digit of %d is %d and is greater than 5\n", ln);
+printf("last digit of %d is %d and is greater than 5\n", n, ln);
 if (ln == 0)
-printf("last digit of %d is %d and is 0\n", ln);
-if (n % 10 < 6 && ln != 0)
-printf("last digit of %d is %d and is less than 6 not 0\n", ln);
+printf("last digit of %d is %d and is 0\n", n, ln);
+if (ln < 6 && ln != 0)
+printf("last digit of %d is %d and is less than 6 not 0\n", n, ln);
 return (0);
 }
 
