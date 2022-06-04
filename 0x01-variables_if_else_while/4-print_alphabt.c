c3d639d7e5adc3d989b553c3969189f1f1dddb0a
Yasin <salim.malee@gmail.com>
2022-06-04 11:54:24 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main print alphabet
+ *
+ * return: 0 (success)
+ */
+
+int main(void);
+{
+int ch;
+ch = 'a';
+while (ch <= 'z')
+{
+	if (ch = 'e')
+	{
+	putchar(++ch);
+	}
+}
+
+return (0);
+
+}
