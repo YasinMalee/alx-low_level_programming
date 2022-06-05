97842eaf9898e1311220879d2cfccf437983cd0d
Yasin <salim.malee@gmail.com>
2022-06-05 08:46:27 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -13,12 +13,11 @@ int main(void)
 
 int ch;
 
-for (ch = 'a'; ch <= 'z'; ++ch)
-
+for (ch = 'z'; ch => 'a'; ++ch)
 {
-        putchar(ch);
+	putchar(ch);
 }
-        putchar('\n');
+putchar('\n');
 
 return (0);
 
