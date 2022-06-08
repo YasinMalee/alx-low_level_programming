75f825676735a5facde28008ac338b9803d9454d
Yasin <salim.malee@gmail.com>
2022-06-08 10:12:09 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table -> time table
+ */
+
+void times_table(void)
+{
+	int x, n, k;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			k = x * n
+				if (n != 0 && k < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+		}
+		_putchar('\n')
+	}
