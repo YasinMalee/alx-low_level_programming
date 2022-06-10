403b10e883c8a906b5d182c869b40c984e885fe3
Yasin <salim.malee@gmail.com>
2022-06-10 11:58:13 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print numbers
+ */
+
+void print_numbers(void)
+{
+	int num
+	for (num = 0; num < 10; num++)
+		_putchar (num +'0');
+	_putchar ('\n');
+}
