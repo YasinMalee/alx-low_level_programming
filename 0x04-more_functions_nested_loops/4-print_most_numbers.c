dc9df204027c543e16c781d88e557c9e7914e1c6
Yasin <salim.malee@gmail.com>
2022-06-10 12:29:10 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,8 @@ void print_most_numbers(void)
 	for (ch = 0; ch < 10; ch++)
 	{
 		if (ch != 2 && ch =! 4)
-
-			_putchar (ch + '0');
+			
+			_putchar(ch + '0');
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
