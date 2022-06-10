2fc6db365f8f0e6157f69a2c640bb81c6236023c
Yasin <salim.malee@gmail.com>
2022-06-10 12:22:28 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that print most numbers
+ */
+
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		if (ch != 2 && ch =! 4)
+
+			_putchar (ch + '0');
+	}
+	_putchar ('\n');
+}
