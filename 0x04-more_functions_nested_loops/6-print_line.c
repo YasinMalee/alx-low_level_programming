3e29f6192c658e3b4838e9108341f839bf1ebe5f
Yasin <salim.malee@gmail.com>
2022-06-10 13:00:21 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - function thst draws straight line
+ * @n: integer parameter
+ */
+
+void print_line(int n)
+{
+	int y;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (y 0; y < n; y++)
+			_putchar(95);
+		_putchar('\n');
+	}
+}
