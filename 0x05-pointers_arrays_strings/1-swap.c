511d81d40735e110969f7bfce9ba360b7fc7a0df
YasinMalee <salim.malee@gmail.com>
2022-06-14 19:15:19 +0300

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- *
+ * swap_int -> give 2 integer swap the variable
+ * @a: parameter 1
+ * @b: parameyer 2
  */
 
 void swap_int(int *a, int *b)
 {
 	int swp;
+
 	swp = *a;
 	*a = *b;
 	*b = swp;
