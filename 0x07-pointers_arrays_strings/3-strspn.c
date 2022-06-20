3c022b547792049e61720943b772e2d61c6310e6
YasinMalee <salim.malee@gmail.com>
2022-06-21 00:35:37 +0300

    get the length of prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - search a string for a set of bytes
+ * @s: source string
+ * @accept: accepted string
+ * Return: number of bytes in the init segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int d = 0, b, c = 0;
+
+	while (accept[d])
+	{
+		b = 0;
+		while (s[b] != 32)
+		{
+			if (accept[d] == s[b])
+			{
+				c++;
+			}
+			b++;
+		}
+		d++;
+	}
+	return (c);
+}
