806d4cf9d7416418a244d6e5b4fdddd7440bd725
YasinMalee <salim.malee@gmail.com>
2022-06-20 23:42:57 +0300

    locate a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-* _strchr -> string character
-* @s: string given
-* @c: second char
-* Return: a string
-*/
+ * _strchr -> string character
+ * @s: string given
+ * @c: second char
+ * Return: a string
+ */
+
 char *_strchr(char *s, char c)
 {
 	int z;
