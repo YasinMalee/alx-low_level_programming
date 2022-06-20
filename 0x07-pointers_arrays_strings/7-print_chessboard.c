7201d5a8b9d60ec2923dbd1f9b9343488de8413c
YasinMalee <salim.malee@gmail.com>
2022-06-21 02:01:42 +0300

    prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_chessboard - Print the chessboard
+  * @a: array of pieces
+  *
+  * Return: Nothing.
+  */
+void print_chessboard(char (*a)[8])
+{
+	int yas, mo;
+
+	for (yas = 0; yas < 8; yas++)
+	{
+		for (mo = 0; mo < 8; mo++)
+		{
+			_putchar(a[yas][mo]);
+		}
+		_putchar('\n');
+	}
+}
