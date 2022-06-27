63820bfb6cdff6f0d19bd079e99296f9a0c8ec05
YasinMalee <salim.malee@gmail.com>
2022-06-27 09:38:44 +0300

    fun print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print its name
+ * @argc: arg parameter
+ * @rgv: array of command line
+ * Return 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])   
+{
+	printf("%s\n ", *argv[v]);
+	printf(0);
+}
