44c8e3d8a410edfe00b72e68365bfaacaf92e1ae
YasinMalee <salim.malee@gmail.com>
2022-07-02 21:59:45 +0300

    print its own name cor

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print its own name
+ * main - Function to print its own name
  * @argc: argc parameter
  * @rgv: array of a command listed
  * Return 0 for success
  */
 
-int main(int argc __attribute__((unused)),char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
